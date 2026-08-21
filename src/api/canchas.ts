51acea4f3559f2a8bd9c22a172da799f4b5cb09f
Ivan Slavkes <ivanslavkes@gmail.com>
2026-08-21 03:14:08 -0300

    Añade getCancha() para una cancha sola

--- src/api/canchas.ts
+++ src/api/canchas.ts
@@ -1,9 +1,14 @@
 import api from "./client";
 import type { Cancha, CanchaInput } from "../types/cancha";
 
-export async function getCanchas(): Promise<Cancha[]> {
+export async function getAllCanchas(): Promise<Cancha[]> {
   const res = await api.get<Cancha[]>("/canchas");
   return res.data;
+}
+
+export async function getCancha(id: number): Promise<Cancha> {
+  const response = await api.get<Cancha>(`/canchas/${id}`);
+  return response.data;
 }
 
 export async function createCancha(data: CanchaInput): Promise<Cancha> {
@@ -11,10 +16,7 @@ export async function createCancha(data: CanchaInput): Promise<Cancha> {
   return res.data;
 }
 
-export async function updateCancha(
-  id: number,
-  data: Partial<CanchaInput>
-): Promise<Cancha> {
+export async function updateCancha(id: number, data: Partial<CanchaInput>): Promise<Cancha> {
   const res = await api.put<Cancha>(`/canchas/${id}`, data);
   return res.data;
 }
